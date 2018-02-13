5e7ba4a96850ce6a9d7eb13b6d146907bf4a66eb
DehouZhang <35927997+DehouZhang@users.noreply.github.com>
2018-02-13 16:44:51 -0600

    Added Alltests.java to run all the units tests

---
+++ app/src/test/java/comp3350/ezcinema/tests/AllTests.java
@@ -0,0 +1,42 @@
+package comp3350.ezcinema.tests;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import comp3350.ezcinema.tests.business.AccessMovieTest;
+import comp3350.ezcinema.tests.business.AccessTheaterTest;
+import comp3350.ezcinema.tests.business.SortMovieTest;
+import comp3350.ezcinema.tests.business.SortTheaterTest;
+import comp3350.ezcinema.tests.objects.MovieTest;
+import comp3350.ezcinema.tests.objects.TheaterTest;
+
+/**
+ * Created by Dehou on 2/13/2018.
+ */
+
+public class AllTests {
+    public static TestSuite suite;
+
+    public static Test suite()
+    {
+        suite = new TestSuite("All tests");
+        testObjects();
+        testBusiness();
+        return suite;
+    }
+
+    private static void testObjects()
+    {
+        suite.addTestSuite(MovieTest.class);
+        suite.addTestSuite(TheaterTest.class);
+    }
+
+    private static void testBusiness()
+    {
+        suite.addTestSuite(AccessMovieTest.class);
+        suite.addTestSuite(AccessTheaterTest.class);
+        suite.addTestSuite(SortTheaterTest.class);
+        suite.addTestSuite(SortMovieTest.class);
+
+    }
+}
