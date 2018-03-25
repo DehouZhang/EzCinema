23bd6ad04c415bf2d914741e0be093666b52311e
Dow <dowgrif@gmail.com>
2018-03-25 16:48:37 -0500

    Made a edittext a textView

--- app/src/main/java/comp3350/ezcinema/presentation/TicketsBoughtActivity.java
+++ app/src/main/java/comp3350/ezcinema/presentation/TicketsBoughtActivity.java
@@ -2,8 +2,11 @@ package comp3350.ezcinema.presentation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -30,10 +33,22 @@ public class TicketsBoughtActivity extends AppCompatActivity {
     {
         ArrayAdapter<Ticket> ticketArrayAdapter;
         ListView ticketList = (ListView)findViewById(R.id.ticketList);
+        TextView noTickets = (TextView)findViewById(R.id.noTicketsTag);
         tickets = new ArrayList<>();
-        ticketManager.getTicketsSequential(tickets);
-        ticketArrayAdapter = new ArrayAdapter<Ticket>(this,android.R.layout.simple_list_item_1,android.R.id.text1, tickets);
-        ticketList.setAdapter(ticketArrayAdapter);
 
+        ticketManager.getTicketsSequential(tickets);
+
+        if(!tickets.isEmpty())
+        {
+            noTickets.setVisibility(View.INVISIBLE);
+            ticketList.setVisibility(View.VISIBLE);
+            ticketArrayAdapter = new ArrayAdapter<Ticket>(this, android.R.layout.simple_list_item_1, android.R.id.text1, tickets);
+            ticketList.setAdapter(ticketArrayAdapter);
+        }
+        else
+        {
+            ticketList.setVisibility(View.INVISIBLE);
+            noTickets.setVisibility(View.VISIBLE);
+        }
     }
 }
